ea2bdd7ae8e3e11d7854eebd70829364a6756903
Project V2 <brandonks337@gmail.com>
2023-09-23 12:24:34 -0500

    first push for NodeJS exercise

---
+++ Exercise3/functions.js
@@ -0,0 +1,23 @@
+// console.log('Hello World!');
+
+function SayHello() {
+    console.log("Hello World Again")
+}
+
+SayHello()
+
+function Sum(A, B) {
+    return A + B
+}
+function Sub(A, B) {
+    return A - B
+}
+function Mult(A, B) {
+    return A * B
+}
+function Div(A, B) {
+    return A / B
+}
+
+Sum(5,10)
+console.log(Sum(2,3))
